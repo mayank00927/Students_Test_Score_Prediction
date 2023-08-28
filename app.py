a2e8c782a4c7b362451e7f11a73c8d02d976113b
Mayank Sharma <96683686+mayank00927@users.noreply.github.com>
2023-08-29 00:20:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
 
